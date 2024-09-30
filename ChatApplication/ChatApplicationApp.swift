f7abecc1f53e92c7c09e2aca70fb9cfc3558d3cf
Sarthak Agrawal <103929885+Sarthak309@users.noreply.github.com>
2024-09-30 10:17:49 +0530

    Initial Commit

---
+++ ChatApplication/ChatApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApplicationApp.swift
+//  ChatApplication
+//
+//  Created by Sarthak Agrawal on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatApplicationTests/ChatApplicationTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatApplicationTests.swift
+//  ChatApplicationTests
+//
+//  Created by Sarthak Agrawal on 30/09/24.
+//
+
+import Testing
+
+struct ChatApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
